8024488cb80fdc76e14eca673aceb53ecd0678af
Krzysztof Werys <krrisgit@gmail.com>
2017-02-19 21:47:07 +0100

    Add Xcode project with a single view application.

---
+++ MetronomeTests/MetronomeTests.swift
@@ -0,0 +1,14 @@
+//
+//  Created by krris on 19/02/2017.
+//  Copyright © 2017 KrrisPl. All rights reserved.
+//
+
+import XCTest
+@testable import Metronome
+
+class MetronomeTests: XCTestCase {
+
+    func testExample() {
+        XCTAssert(true)
+    }
+}
