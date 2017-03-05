66d7ab13bbe33735c5b3765cd19ae763fcb867c9
Krzysztof Werys <krrisgit@gmail.com>
2017-03-05 10:59:25 +0100

    Make UIView reusable and load it in Storyboard.

---
+++ Metronome/BeatView.swift
@@ -0,0 +1,13 @@
+//
+//  BeatView.swift
+//  Metronome
+//
+//  Created by krris on 02/03/2017.
+//  Copyright © 2017 KrrisPl. All rights reserved.
+//
+
+import UIKit
+
+final class BeatView: UIView, Reusable {
+    let nibName: String = "BeatView"
+}
---
+++ Metronome/UIView.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// Make UIView reusable by loading a view from .xib
+protocol Reusable {
+    // A name of the .xib file
+    var nibName: String { get }
+}
+
+extension UIView {
+    open override func awakeAfter(using aDecoder: NSCoder) -> Any? {
+        if let reusableView = self as? Reusable {
+            return substituteWithFirstViewFromNibNamed(nibName: reusableView.nibName,
+                                                       onCondition: subviews.count == 0)
+        }
+
+        return self
+    }
+
+    private func substituteWithFirstViewFromNibNamed(nibName: String, onCondition condition: @autoclosure () -> Bool) -> UIView {
+        if condition() {
+            let nib = UINib(nibName: nibName, bundle: nil)
+            if let view = nib.instantiate(withOwner: nil, options: nil).first as? UIView {
+                view.translatesAutoresizingMaskIntoConstraints = self.translatesAutoresizingMaskIntoConstraints
+                return view
+            }
+        }
+
+        return self
+    }
+}
