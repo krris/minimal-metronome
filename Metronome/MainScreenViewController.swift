b9c73e96c3d0dd487fc1196faefa71316614b7c8
Krzysztof Werys <krrisgit@gmail.com>
2017-03-28 20:59:00 +0200

    Play scheduled beat.

--- Metronome/MainScreenViewController.swift
+++ Metronome/MainScreenViewController.swift
@@ -3,17 +3,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MainScreenViewController: UIViewController {
 
     public var viewModel: MainScreenViewModel!
     @IBOutlet private weak var beatLabel: BeatLabel!
+    private var metronome: Metronome?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         assert(viewModel != nil)
         beatLabel.configureWith(viewModel: viewModel.beatViewModel)
+
+        let fileUrl = Bundle.main.url(forResource: "tick", withExtension: "wav")!
+        metronome = Metronome(fileURL: fileUrl)
+        metronome?.play(bpm: 90)
+
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Metronome/Metronome.swift
@@ -0,0 +1,68 @@
+//
+//  Copyright © 2017 KrrisPl. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class Metronome {
+
+    private let tickAudioFile: AVAudioFile
+    private let audioPlayerNode: AVAudioPlayerNode
+    private let audioEngine: AVAudioEngine
+
+    init?(fileURL: URL) {
+
+        do {
+            tickAudioFile = try AVAudioFile(forReading: fileURL)
+        } catch {
+            fatalError("Cannot load an audio file: \(error)")
+        }
+
+        audioPlayerNode = AVAudioPlayerNode()
+
+        audioEngine = AVAudioEngine()
+        audioEngine.attach(self.audioPlayerNode)
+        audioEngine.connect(audioPlayerNode, to: audioEngine.mainMixerNode, format: tickAudioFile.processingFormat)
+
+        do {
+            try audioEngine.start()
+        } catch {
+            fatalError("Cannot start an audio engine \(error)")
+        }
+    }
+
+    // MARK: API
+
+    func play(bpm: Int) {
+
+        let buffer = generateBuffer(bpm: bpm)
+
+        if audioPlayerNode.isPlaying {
+            audioPlayerNode.scheduleBuffer(buffer, at: nil, options: .interruptsAtLoop, completionHandler: nil)
+        } else {
+            self.audioPlayerNode.play()
+        }
+
+        self.audioPlayerNode.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
+    }
+
+    func stop() {
+        audioPlayerNode.stop()
+    }
+
+    // MARK: Private
+
+    private func generateBuffer(bpm: Int) -> AVAudioPCMBuffer {
+        tickAudioFile.framePosition = 0
+        let periodLength = AVAudioFrameCount(tickAudioFile.processingFormat.sampleRate * 60 / Double(bpm))
+        let buffer = AVAudioPCMBuffer(pcmFormat: tickAudioFile.processingFormat, frameCapacity: periodLength)
+        do {
+            try tickAudioFile.read(into: buffer)
+        } catch {
+            fatalError("Cannot create a buffer: \(error)")
+        }
+        buffer.frameLength = periodLength
+        return buffer
+    }
+}
