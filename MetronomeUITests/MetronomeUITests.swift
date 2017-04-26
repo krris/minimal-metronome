7c11e31b48d7070b8c6ebb0ae1472ed8e614070d
Krzysztof Werys <krrisgit@gmail.com>
2017-04-26 22:19:03 +0200

    Add UI tests which chech if swiping changes the value of BPM

--- MetronomeUITests/MetronomeUITests.swift
+++ MetronomeUITests/MetronomeUITests.swift
@@ -13,9 +13,24 @@ class MetronomeUITests: XCTestCase {
         XCUIApplication().launch()
     }
 
-    func testExample() {
+    func testIfLabelAndGradientIsDisplayed() {
         let app = XCUIApplication()
         XCTAssertTrue(app.staticTexts["beats-per-minute"].exists)
         XCTAssertTrue(app.otherElements["gradient"].exists)
+    }
+
+    func testIfSwipingChangesBeatsPerMinute() {
+        let app = XCUIApplication()
+        let element = app.otherElements["gradient"].children(matching: .other).element
+        let initialBpm = Int(app.staticTexts["beats-per-minute"].label)!
+
+        element.swipeUp()
+        let higherBpm = Int(app.staticTexts["beats-per-minute"].label)!
+        assert(initialBpm < higherBpm)
+
+        element.swipeDown()
+        let lowerBpm = Int(app.staticTexts["beats-per-minute"].label)!
+        assert(lowerBpm < higherBpm)
+
     }
 }
